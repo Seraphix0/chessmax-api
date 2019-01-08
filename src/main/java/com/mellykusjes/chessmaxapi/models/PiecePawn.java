00802c9135b3eca69dfb0d78b9112b124375c5d3
Melvin <melvinkusters0@gmail.com>
2019-01-08 12:08:44 +0100

    WIP computePawnMove()

--- src/main/java/com/mellykusjes/chessmaxapi/models/PiecePawn.java
+++ src/main/java/com/mellykusjes/chessmaxapi/models/PiecePawn.java
@@ -1,5 +1,9 @@
 package com.mellykusjes.chessmaxapi.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class PiecePawn extends Piece {
 
     private boolean notMoved = true;
@@ -7,5 +11,16 @@ public class PiecePawn extends Piece {
     @Override
     public void setType() {
         System.out.println("Adding Piece of type 'Pawn' with id: " + getId());
+    }
+
+    @Override
+    public List<Position> computeAllowedTargetPositions(Map<Position, Piece> boardState, Position initialPosition) {
+        return computePawnMove(boardState, initialPosition);
+    }
+
+    private List<Position> computePawnMove(Map<Position, Piece> boardState, Position initialPosition) {
+        List<Position> allowedTargetPositions = new ArrayList<>();
+
+        
     }
 }
